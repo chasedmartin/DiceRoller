0721e226af9088dfb1b133c17e6d70f643e23eb6
Chase Martin <chasedmartin@gmail.com>
2023-05-04 08:19:14 -0500

    Initial commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Chase Martin on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
